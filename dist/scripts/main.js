03659afc215722b5aeb9add1e69f4f22de58bd0a
Rob <rdaly1490@gmail.com>
2015-06-01 14:56:03 -0500

    done maybe add some effects

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -9,7 +9,7 @@ $("#intro").hide();
 			"play": "foo",
 			"leader": "bar",
 			"settings": "baz",
-			"*not-found": "dne"
+			"*notfound": "dne"
 		},
 
 		menu: function() {
@@ -22,38 +22,31 @@ $("#intro").hide();
 			$("#set").hide();
 			$("#intro").show();
 			$("#intro").css("marginTop", "200px");
-			$("#menu").css("backgroundColor", "#6e2929");
-
 		},
 
 		foo: function() {
-			console.log("Play");
 			$(".page").hide();
 			$("#intro").hide();
 			$("#play-game").show();
-			$("#menu").css("backgroundColor", "#6e2929");
 		},
 
 		bar: function() {
-			console.log("Leaderboards");
 			$(".page").hide();
 			$("#intro").hide();
 			$("#leaderboards").show();
-			$("#menu").css("backgroundColor", "#6e2929");
 		},
 
 		baz: function() {
-			console.log("settings");
 			$(".page").hide();
 			$("#intro").hide();
 			$("#set").show();
-			$("#menu").css("backgroundColor", "#6e2929");
 		},
 
 		dne: function() {
-			$("#menu").hide();
-			$("#into p").innerHTML("404");
-			$("#into p p").innerHTML("Page Not Found");
+			$(".page").hide();
+			$("#intro #top").html("404 Page Not Found");
+			$("#intro #bottom").html("");
+			$("#intro").css("marginTop", "200px");
 			$("#intro").show();
 		}
 
